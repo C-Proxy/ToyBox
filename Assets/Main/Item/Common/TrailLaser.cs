9a599a3885f02744483951560441c49d6714b88b
CProxy <nc2.7182818284590@gmail.com>
2021-08-07 11:10:05 +0900

    perf: TrailLaser Queue

--- Assets/Main/Item/Common/TrailLaser.cs
+++ Assets/Main/Item/Common/TrailLaser.cs
@@ -10,61 +10,32 @@ using Cysharp.Threading.Tasks.Linq;
 [RequireComponent(typeof(LineRenderer))]
 public class TrailLaser : LocalPoolableChildBehaviour
 {
-    [SerializeField] float m_DisplayTime = 1f;
+    [SerializeField] int m_MaxCount;
     LineRenderer m_LineRenderer;
-    Queue<Vector3[]> m_PositionsQueue;
+    Queue<Vector3[]> m_PointsQueue;
 
-    CancellationTokenSource m_RemoveCTS;
     override public void Init()
     {
         m_LineRenderer = GetComponent<LineRenderer>();
     }
     override public void OnSpawn()
     {
-        m_PositionsQueue = new Queue<Vector3[]>();
-        AddPosition(transform.position);
-        m_RemoveCTS = new CancellationTokenSource();
-        RemoveAsync(m_RemoveCTS.Token);
+        m_PointsQueue = new Queue<Vector3[]>();
     }
     override public void OnPool()
     {
-        ClearPositions();
-        m_PositionsQueue = null;
+        m_PointsQueue.Clear();
+        m_PointsQueue = null;
         m_LineRenderer.positionCount = 0;
-        m_RemoveCTS?.Cancel();
-        m_RemoveCTS = null;
     }
     public void AddPosition(Vector3 position)
     => AddPositions(new[] { position });
     public void AddPositions(Vector3[] positions)
     {
-        m_PositionsQueue.Enqueue(positions);
-        UpdateLaser();
-    }
-    public void RemovePosition()
-    {
-        m_PositionsQueue.Dequeue();
-        UpdateLaser();
-    }
-    public void ClearPositions() => m_PositionsQueue.Clear();
-    async void RemoveAsync(CancellationToken token)
-    {
-        try
-        {
-            await foreach (var _ in UniTaskAsyncEnumerable.EveryUpdate())
-            {
-                await UniTask.Delay(TimeSpan.FromSeconds(m_DisplayTime), false, PlayerLoopTiming.Update, token);
-                RemovePosition();
-            }
-        }
-        catch
-        {
-
-        }
-    }
-    void UpdateLaser()
-    {
-        var array = m_PositionsQueue.SelectMany(_ => _).ToArray();
+        m_PointsQueue.Enqueue(positions);
+        if (m_PointsQueue.Count > m_MaxCount)
+            m_PointsQueue.Dequeue();
+        var array = m_PointsQueue.SelectMany(_ => _).ToArray();
         m_LineRenderer.positionCount = array.Length;
         m_LineRenderer.SetPositions(array);
     }
