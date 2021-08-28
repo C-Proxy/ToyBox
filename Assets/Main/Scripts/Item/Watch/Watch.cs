8aadfca728a07720caf49e8f1a88fb1c22cbabf8
CProxy <nc2.7182818284590@gmail.com>
2021-08-28 13:05:43 +0900

    feat: Watch

---
+++ Assets/Main/Scripts/Item/Watch/Watch.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Linq;
+
+public class Watch : MonoBehaviour
+{
+    [SerializeField] Transform m_ShortAnchor = default, m_LongAnchor = default;
+    private void Awake()
+    {
+        CountAsync(gameObject.GetCancellationTokenOnDestroy()).Forget();
+    }
+    async UniTaskVoid CountAsync(CancellationToken token)
+    {
+        var countEnumerable = UniTaskAsyncEnumerable.Create<(Quaternion ShortRot, Quaternion LongRot)>(async (writer, token) =>
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var dateTime = DateTime.Now;
+                var minute = dateTime.Minute + dateTime.Second / 60f;
+                var hour = dateTime.Hour + minute / 60f;
+                Debug.Log($"Minute:{minute},Hour:{hour}");
+                await writer.YieldAsync((Quaternion.Euler(0, hour * 30f, 0), Quaternion.Euler(0, minute * 6f, 0)));
+                await UniTask.Delay(TimeSpan.FromSeconds(1), cancellationToken: token);
+            }
+        });
+        await foreach (var (shortRot, longRot) in countEnumerable.WithCancellation(token))
+        {
+            m_ShortAnchor.localRotation = shortRot;
+            m_LongAnchor.localRotation = longRot;
+        }
+    }
+}
