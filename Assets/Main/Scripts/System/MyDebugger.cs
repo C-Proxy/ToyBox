158ed63e3b8d832a6656c8191a6d1a66b7eabda9
CProxy <nc2.7182818284590@gmail.com>
2021-09-04 08:06:29 +0900

    feat: GrabDebug

--- Assets/Main/Scripts/System/MyDebugger.cs
+++ Assets/Main/Scripts/System/MyDebugger.cs
@@ -9,14 +9,12 @@ using CoinSpace;
 
 public class MyDebugger : MonoBehaviour
 {
-    IObservable<Unit> m_UpdateAsObservable;
-    IObservable<Unit> KeyDownAsObservable(KeyCode key) => m_UpdateAsObservable.Where(_ => Input.GetKeyDown(key));
-    private void Awake()
+    [SerializeField] BaseGrabber Grabber = default;
+    [SerializeField] BaseItem TargetItem = default;
+    [ContextMenu("ForceGrab")]
+    public void ForceGrab()
     {
-        // m_UpdateAsObservable = this.UpdateAsObservable().Publish().RefCount();
-        // KeyDownAsObservable(KeyCode.Q).Subscribe(_ => PlayingCardStacker.GenerateFullDeck(Vector3.up, default)).AddTo(this);
-        // KeyDownAsObservable(KeyCode.W).Subscribe(_ => CoinStacker.GenerateCoins(Vector3.up, default, Coinage.Ten, 30)).AddTo(this);
-        // KeyDownAsObservable(KeyCode.E).Subscribe(_ => PokerBoard.Generate()).AddTo(this);
-        // KeyDownAsObservable(KeyCode.R).Subscribe(_ => Dice.Generate()).AddTo(this);
+        if (Grabber?.NetworkBehaviour)
+            TargetItem?.TryGrabServerRpc(NetworkInfo.CreateFrom(Grabber.NetworkBehaviour));
     }
 }
